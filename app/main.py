b0e36b67fc5db2d1efe8be748fb7cb81146ee59a
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-05-27 01:43:26 +0800

    test faas upload

--- app/main.py
+++ app/main.py
@@ -1,22 +1,35 @@
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse #allow html response
 from fastapi.templating import Jinja2Templates #allow renderhtml
+from fastapi.staticfiles import StaticFiles
 
 import shutil #copy uploaded files
 import os
+
 #import engine (runner.py)
 from app.runner import run_function
 
+
 #created Fastapi instance
-app = FastAPI
+app = FastAPI()
+
+
+app.mount(
+    "/static",
+    StaticFiles(directory="static"),
+    name="static"
+)
+
 
 #locate templates
-templates = Jinja2Templates (
-    directory = "templates"
+templates = Jinja2Templates(
+    directory="templates"
 )
+
 
 #this wjhere uploaded functions be stored
 FUNCTIONS_DIR = "functions"
+
 
 #if this folder doesnt exost yet we will create one
 os.makedirs(
@@ -24,68 +37,79 @@ os.makedirs(
     exist_ok=True
 )
 
-    #homeoute/ diplay dashboard
+
+#homeoute/ diplay dashboard
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
 
     #get all uploaded funct/files
-    files = os.listdir (FUNCTIONS_DIR)
+    files = os.listdir(FUNCTIONS_DIR)
 
+    return templates.TemplateResponse(
 
+        request,
 
-    return templates.TemplatesResponse (
         #html file to render
         "index.html",
 
         #data sent to template
         {
-            "request": request,
             "files": files # data sent to template
         }
-        
+
     )
+
+
 #deploy function route
-@app.post ("/deploy")
-async def deploy_function (
+@app.post("/deploy")
+async def deploy_function(
+
     file: UploadFile = File(...) #receive uploaded file
 ):
+
     #build full file path
-    filepath = os.path.jpin(
+    filepath = os.path.join(
         FUNCTIONS_DIR,
         file.filename
     )
+
     #save uploaded files to disk| wb => write binary mode
-    with open (filepath, "wb") as buffer:
+    with open(filepath, "wb") as buffer:
 
         shutil.copyfileobj(
             file.file,
             buffer
-
         )
 
     #return deployment response
     return {
+
         "message": "functions have been deployed",
+
         "filename": file.filename
     }
-#execute function route(trigger upload function)
 
+
+#execute function route(trigger upload function)
 @app.get("/execute/{filename}")
-async def execute_function(filename:str):
+async def execute_function(filename: str):
 
     #build file path
     filepath = os.path.join(
         FUNCTIONS_DIR,
         filename
     )
+
     #prevent executing missing functions
     if not os.path.exists(filepath):
+
         return {
             "error": "Aww Snap! Function isn't found"
         }
+
     #run uploaded function....
     #trigger doc container/funct .exec/container deletion kaboom
-    result = run_function (filepath)
+    result = run_function(filepath)
 
     #return .exec output logs
     return result
