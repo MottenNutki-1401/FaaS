d6f57c6daeb3a75f092d390f48c112ed30d3ffb1
MottenNutki-1401 <202310763@gordoncollege.edu.ph>
2026-05-26 22:38:50 +0800

    added execution runner

---
+++ app/runner.py
@@ -0,0 +1,44 @@
+import subprocess # allow py exec in termial, use to run docker subprocess
+import uuid # generate randomid, prevent container conflicts
+import os #file paths
+
+
+#function for exec
+#uploads user functuiob inside docker container
+def run_function(filepath):
+    container_name = f"faas-{uuid.uuid4().hex[:8]}" # takes only 8 chars
+
+#command build (docker as python list)
+
+    command = [
+        "docker", #docker executable
+        "run", #run a new container
+        "--rm", #delete container after execution
+        "--name",
+        container_name,
+
+        #mount in container
+        "-v",
+        f"{os.path.abspath(filepath)}:/app/function.py",
+
+        #create isolated python environmnet
+        "python:3.11",
+
+        #run uploaded python function
+        "python",
+        "/app/function.py" #filepath inside container
+
+    ]
+    #execute docker command
+    result = subprocess.run(
+        command,
+        capture_output =True,
+        text=True
+    )
+    #return execution results 
+    return {
+        "stdout":result.stdout, #normal program output
+        "stderr": result.stderr, #error output
+        "returncode": result.returncode # status code
+
+    }
